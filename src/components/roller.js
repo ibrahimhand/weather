c375128162e3a1a73932a62a530755d63fc9a7db
ibrahimhand <ibramhand555@gmail.com>
2022-11-16 11:58:04 +0100

    edit

---
+++ src/components/roller.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../components/css/Roller.css";
+
+const Roller = () => {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Roller;
